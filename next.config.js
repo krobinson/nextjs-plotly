76b3d0bac7c17928c933a384f45ca0be84a50d42
Ken Robinson <kenrobinsonter@gmail.com>
2024-01-09 08:00:48 +0000

    Got DevContainer for next.js up and going. Todo wire up applicaton to get to backend.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+    rewrites: async () => {
+      return [
+        {
+          source: "/api/:path*",
+          destination:
+            process.env.NODE_ENV === "development"
+              ? "http://localhost:8000/api/:path*"
+              : "/api/",
+        },
+      ];
+    },
+  };
+  
+module.exports = nextConfig;
